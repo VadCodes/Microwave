21c7e7301b1aa042e35f7757339a822e7ab6221b
vadoche <vadim.cote.1@ulaval.ca>
2015-10-29 17:57:53 -0400

    Modifie paire parcours

--- SimulatHEURE/src/BesoinsTransport/paireParcours.java
+++ SimulatHEURE/src/BesoinsTransport/paireParcours.java
@@ -12,10 +12,22 @@ import java.util.LinkedList;
  * @author vadimcote
  */
 public class paireParcours {
-    private final ParcoursPieton m_parcoursPieton;
-    private final ParcoursBus m_parcoursBus;
+    private ParcoursPieton m_parcoursPieton;
+    private ParcoursBus m_parcoursBus;
     public paireParcours(ParcoursPieton m_parcoursPieton, ParcoursBus m_parcoursBus){
         this.m_parcoursPieton = m_parcoursPieton;
         this.m_parcoursBus = m_parcoursBus;
+    }
+    public ParcoursPieton getParcoursPieton(){
+        return m_parcoursPieton;
+    }
+    public ParcoursBus getParcoursBus(){
+        return m_parcoursBus;
+    }
+    public void setParcoursPieton(ParcoursPieton parcoursPieton){
+        m_parcoursPieton = parcoursPieton;
+    }
+    public void setParcoursBus(ParcoursBus parcoursBus){
+        m_parcoursBus = parcoursBus;
     }
 }
