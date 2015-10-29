60d537f24375542c6a072db5107f2b2086424383
nasim80 <n.s222att@hotmail.com>
2015-10-29 18:11:52 -0400

    yoplait

---
+++ SimulatHEURE/src/ReseauRoutier/Trajet.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ReseauRoutier;
+
+/**
+ *
+ * @author Nathaniel
+ */
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Trajet {
+    private Emplacement m_emplacementInitial;
+    private Emplacement m_emplacementFinal;
+    private LinkedList<Troncon> m_listTroncons;
+    public Trajet(Emplacement emplacementInitial, Emplacement emplacementFinal, LinkedList<Troncon> listTroncons){
+        m_emplacementInitial = emplacementInitial;
+        m_emplacementFinal = emplacementFinal;
+        m_listTroncons = listTroncons;
+    }
+    public Boolean estValid(){
+        Boolean tmp1;
+        Boolean tmp2;
+        tmp1 = m_listTroncons.getFirst().equals(m_emplacementInitial.getTroncon());
+        tmp2 = m_listTroncons.getLast().equals(m_emplacementFinal.getTroncon());
+        Iterator<Troncon> itr = m_listTroncons.iterator();
+        if(itr.hasNext()){
+            Troncon theLastOne = itr.next();
+            while(itr.hasNext()){
+                 Troncon tmp = itr.next();
+                 if(theLastOne.estAdjacent(tmp)){
+                     return false;
+                 }
+                 theLastOne = tmp;
+             }
+        }
+        return true;
+    }
+}
---
+++ SimulatHEURE/test/ReseauRoutier/EmplacementTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ReseauRoutier;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Nathaniel
+ */
+public class EmplacementTest extends TestCase {
+    
+    public EmplacementTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Test of calculPosition method, of class Emplacement.
+     */
+    public void testCalculPosition() {
+        Position position1 = new Position(5,10);
+        Position position2 = new Position(5,10);
+        
+        
+    }
+}
