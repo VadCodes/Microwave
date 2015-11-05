4d6794f11f90425977a309bcceb508d17d4bca32
Louis Fortier-Dubois <louisfd94@gmail.com>
2015-11-05 17:06:27 -0500

    jtrouvais qui était temps que jfasse un commit

--- SimulatHEURE/src/ReseauTransport/SourceAutobus.java
+++ SimulatHEURE/src/ReseauTransport/SourceAutobus.java
@@ -10,5 +10,21 @@ package ReseauTransport;
  * @author louis
  */
 public class SourceAutobus {
+    private String m_nomSource;
+    private int m_nbAutobusGeneres = 0;
+    private Circuit m_circuit;
+    private Emplacement m_emplacement;
     
+    public void genererAutobus(){
+        //Autobus nouvelAutobus = new Autobus(estSurArret());
+        m_circuit.assignerTrajetAutobus(nouvelAutobus);
+    }
+    
+    public String genererBusID(){
+        return m_nomSource + Integer.toString(m_nbAutobusGeneres);
+    }
+        
+    public Boolean estSurArret(){
+        return m_emplacement.equals(m_circuit.getListeArretTrajet().getFirst().getArret().getEmplacement()); 
+    }
 }
