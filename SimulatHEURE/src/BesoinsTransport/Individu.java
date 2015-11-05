200e681374478e5e44d6b5b56bf1b6593c56eae4
vadoche <vadim.cote.1@ulaval.ca>
2015-11-05 15:52:20 -0500

    ok1

--- SimulatHEURE/src/BesoinsTransport/Individu.java
+++ SimulatHEURE/src/BesoinsTransport/Individu.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package BesoinsTransport;
+import ReseauRoutier.Emplacement;
+import java.util.ListIterator;
+import Utilitaire.Temps;
 
 /**
  *
  * @author vadimcote
  */
 public class Individu {
+    private ListIterator<PaireParcours> m_iterateurItineraire;
+    private Emplacement m_emplacementActuel;
+    private Itineraire m_itineraire;
+    private Temps m_tempsApparition;
+    private Boolean estSurParcoursPieton;
+    private PaireParcours m_paireActuelle;
+    public Individu(Emplacement p_emplacementActuel, Itineraire p_itineraire, Temps p_temps){
+        m_emplacementActuel = p_emplacementActuel;
+        m_itineraire = p_itineraire;
+        m_tempsApparition = p_temps;
+        
+        m_iterateurItineraire = m_itineraire.getListPaireParcours().listIterator(0);
+        m_paireActuelle = m_itineraire.getListPaireParcours().getFirst();
+    }
+    public void incrementeItineraire(){
+        m_paireActuelle = m_iterateurItineraire.next();
+    }
+    
+    
+    
+    
+            
     
 }
