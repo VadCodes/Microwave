d661238ba9da71181c7e6d0017860c26649d4dd3
NASIM80 <NASIM80@IFT-3920-27.fsg.ulaval.ca>
2015-11-05 14:52:49 -0500

    tabarnak vinny 3

---
+++ SimulatHEURE/src/Controleur/Simulatheure.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleur;
+import ReseauRoutier.ReseauRoutier;
+import ReseauTransport.ReseauTransport;
+import BesoinsTransport.BesoinTransport;
+
+import java.util.LinkedList;
+
+
+/**
+ *
+ * @author vinny
+ */
+public class Simulatheure {
+    private ReseauRoutier m_reseauRoutier;
+    private ReseauTransport m_reseauTransport;
+    private LinkedList<BesoinTransport> m_listBesoins;
+    
+    public Simulatheure() {
+        
+    }
+    
+    public Simulatheure() {
+        
+    }
+    
+}
