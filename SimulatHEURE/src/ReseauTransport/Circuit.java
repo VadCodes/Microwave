1681d96c78b3b1e28681fa4a0fec00a6f00133b8
Louis Fortier-Dubois <louisfd94@gmail.com>
2015-11-05 15:15:20 -0500

    cum it

--- SimulatHEURE/src/ReseauTransport/Circuit.java
+++ SimulatHEURE/src/ReseauTransport/Circuit.java
@@ -4,20 +4,22 @@
  * and open the template in the editor.
  */
 package ReseauTransport;
-import java.util.LinkedList;
+
 
 /**
  *
  * @author louis
  */
 import java.util.LinkedList;
+import java.util.ListIterator;
+import ReseauRoutier.Position;
 
 public class Circuit {
-    String nom = "";
-    Arret Origine;
-    LinkedList<SourceAutobus> listeSources;
-    LinkedList<Autobus> listeAutobus;
-    LinkedList<PaireArretTrajet> listeArretTrajet;
+    private String nom = "";
+    private Arret Origine;
+    private LinkedList<SourceAutobus> listeSources;
+    private LinkedList<Autobus> listeAutobus;
+    private LinkedList<PaireArretTrajet> listeArretTrajet;
     
     public void ajouterSource(SourceAutobus source){
         listeSources.add(source);
@@ -29,5 +31,43 @@ public class Circuit {
     
     public void calculCirculationGlobal(){
         
+        //on vide toutes les files d'arrets
+        ListIterator<PaireArretTrajet> arretTrajetItr = listeArretTrajet.listIterator();
+        while (arretTrajetItr.hasNext()) {
+            arretTrajetItr.next().getArret().viderFile();
+        }
+        
+        //pour chaque autobus on calcule la circulation
+        ListIterator<Autobus> autobusItr = listeAutobus.listIterator();
+        while (autobusItr.hasNext()) {
+            calculCirculation(autobusItr.next());
+        }
+    }
+    
+    public void calculCirculation(Autobus bus){
+        
+    }
+    
+    public LinkedList<Position> getPositionsDesAutobus(){
+        
+        LinkedList<Position> listePositionsAutobus = new LinkedList<Position>();
+        ListIterator<Autobus> autobusItr = listeAutobus.listIterator();
+        while (autobusItr.hasNext()) {
+            listePositionsAutobus.add(autobusItr.next().getPosition());
+        }
+        
+        return listePositionsAutobus;
+    }
+    
+    public LinkedList<PaireArretTrajet> getListeArretTrajet(){
+        return listeArretTrajet;
+    }
+    
+    public void incrementerIterateurAutobus(Autobus bus){
+        bus.incrementerIterateur();
+    }
+    
+    public void assignerTrajetAutobus(Autobus bus){
+        bus.assignerTrajet(listeArretTrajet);
     }
 }
--- SimulatHEURE/src/ReseauTransport/PaireArretTrajet.java
+++ SimulatHEURE/src/ReseauTransport/PaireArretTrajet.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package ReseauTransport;
+import ReseauRoutier.Trajet;
 
 /**
  *
  * @author louis
  */
 public class PaireArretTrajet {
+    private Arret m_arret;
+    private Trajet m_trajet;
     
+    public PaireArretTrajet(Arret arret, Trajet trajet){
+            m_arret = arret;
+            m_trajet = trajet;
+    }
+    
+    public Arret getArret(){
+        return m_arret;
+    }
+
+    public Trajet getTrajet(){
+        return m_trajet;
+    }
 }
