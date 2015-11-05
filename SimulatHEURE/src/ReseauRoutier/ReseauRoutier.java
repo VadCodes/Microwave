7ee21ca466605debd68024e1cf89c584b4f0d9ed
NASIM80 <NASIM80@IFT-3920-27.fsg.ulaval.ca>
2015-11-05 13:52:26 -0500

    vinny job

---
+++ SimulatHEURE/src/ReseauRoutier/ReseauRoutier.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ReseauRoutier;
+
+/**
+ *
+ * @author NASIM80
+ */
+public class ReseauRoutier {
+    private ReseauRoutierFactory m_reseauRoutier;
+}
