ccc663a3f515bf9d56ded6eaa34cad8c096d4469
nasim80 <n.s222att@hotmail.com>
2015-11-08 12:04:51 -0500

    Hey yo sourceAUtobus terminer bitches

--- SimulatHEURE/src/ReseauTransport/SourceAutobus.java
+++ SimulatHEURE/src/ReseauTransport/SourceAutobus.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package ReseauTransport;
+import ReseauRoutier.Emplacement;
+import Utilitaire.Temps;
 
 /**
  *
@@ -14,10 +16,31 @@ public class SourceAutobus {
     private int m_nbAutobusGeneres = 0;
     private Circuit m_circuit;
     private Emplacement m_emplacement;
+    private Temps m_frequence;
+    private Temps m_tempsAttenteinitial;
+    private Temps m_tempsAvantApparition;
+    public SourceAutobus(Emplacement p_emplacement, Circuit p_circuit, String p_nomSource, Temps p_frequence, Temps p_tempsAttenteinitial){
+        m_emplacement = p_emplacement;
+        m_circuit = p_circuit;
+        m_nomSource = p_nomSource;
+        m_frequence = p_frequence;
+        m_tempsAttenteinitial = p_tempsAttenteinitial;
+        m_tempsAvantApparition = m_tempsAttenteinitial;
+    }
     
-    public void genererAutobus(){
-        //Autobus nouvelAutobus = new Autobus(estSurArret());
-        m_circuit.assignerTrajetAutobus(nouvelAutobus);
+    public void miseAjoutTempsRestant(Temps p_deltatT){
+        double tmp = m_tempsAvantApparition.getTemps() - p_deltatT.getTemps();
+        m_tempsAvantApparition = new Temps(tmp);
+    }
+    public void genererAutobus(int p_capaciteMax){
+        while(m_tempsAvantApparition.getTemps() <= 0){
+            m_nbAutobusGeneres++;
+            Temps tempsApparition = tempsApparition();
+            Autobus nouvelAutobus = new Autobus(m_emplacement, p_capaciteMax, genererBusID(),tempsApparition ,estSurArret() );
+            m_circuit.assignerTrajetAutobus(nouvelAutobus);
+            double tmp = m_tempsAvantApparition.getTemps() + m_frequence.getTemps();
+            m_tempsAvantApparition = new Temps(tmp);
+        }
     }
     
     public String genererBusID(){
@@ -26,5 +49,11 @@ public class SourceAutobus {
         
     public Boolean estSurArret(){
         return m_emplacement.equals(m_circuit.getListeArretTrajet().getFirst().getArret().getEmplacement()); 
+    }
+    
+    public Temps tempsApparition(){
+       double tmp =  m_tempsAttenteinitial.getTemps()+ (m_frequence.getTemps() * m_nbAutobusGeneres);
+        Temps tmo = new Temps(tmp);
+        return tmo;
     }
 }
