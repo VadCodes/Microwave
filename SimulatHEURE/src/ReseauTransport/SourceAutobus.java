c3cd567f5936659bad8d461ee13df3d3b6543693
nasim80 <n.s222att@hotmail.com>
2015-11-08 12:28:25 -0500

    correction source autobus

--- SimulatHEURE/src/ReseauTransport/SourceAutobus.java
+++ SimulatHEURE/src/ReseauTransport/SourceAutobus.java
@@ -25,11 +25,12 @@ public class SourceAutobus {
     private Temps m_frequence;
     private Temps m_tempsAttenteinitial;
     private Temps m_tempsAvantApparition;
-    public SourceAutobus(Emplacement p_emplacement, Circuit p_circuit, String p_nomSource, Temps p_frequence, Temps p_tempsAttenteinitial){
+    
+    public SourceAutobus(Emplacement p_emplacement, Circuit p_circuit, String p_nomSource, Distribution p_distribution,  Temps p_tempsAttenteinitial){
         m_emplacement = p_emplacement;
         m_circuit = p_circuit;
         m_nomSource = p_nomSource;
-        m_frequence = p_frequence;
+        m_distibutionFrequence = p_distribution;
         m_tempsAttenteinitial = p_tempsAttenteinitial;
         m_tempsAvantApparition = m_tempsAttenteinitial;
     }
@@ -38,19 +39,16 @@ public class SourceAutobus {
         double tmp = m_tempsAvantApparition.getTemps() - p_deltatT.getTemps();
         m_tempsAvantApparition = new Temps(tmp);
     }
-    public void genererAutobus(int p_capaciteMax){
+    public void genererAutobus(){
         while(m_tempsAvantApparition.getTemps() <= 0){
-            m_nbAutobusGeneres++;
-            Temps tempsApparition = tempsApparition();
-            Autobus nouvelAutobus = new Autobus(m_emplacement, p_capaciteMax, genererBusID(),tempsApparition ,estSurArret() );
+            miseAjourAvantAjout();
+            Autobus nouvelAutobus = new Autobus(m_emplacement, m_capaciteMax,  genererBusID(),tempsApparition() ,estSurArret() );
             m_circuit.ajouterAutobus(nouvelAutobus);
             m_circuit.assignerTrajetAutobus(nouvelAutobus);
-            
             double tmp = m_tempsAvantApparition.getTemps() + m_frequence.getTemps();
             m_tempsAvantApparition = new Temps(tmp);
         }
     }
-    
     
     public String genererBusID(){
         m_nbAutobusGeneres++;
@@ -73,5 +71,9 @@ public class SourceAutobus {
     
     public void initSourceAutobus(){
         m_frequence = m_distibutionFrequence.pigerTemps();
+    }
+    public void miseAjourAvantAjout(){
+        m_frequence  = m_distibutionFrequence.pigerTemps();
+        m_nbAutobusGeneres++;
     }
 }
