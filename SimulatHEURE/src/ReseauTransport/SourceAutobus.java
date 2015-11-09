e1383eb7eed8f4a88b8b7b0acdd184687b56efc5
Vincent Martel <Vincent Martel@P8Z68-V>
2015-11-08 21:38:47 -0500

    kk

--- SimulatHEURE/src/ReseauTransport/SourceAutobus.java
+++ SimulatHEURE/src/ReseauTransport/SourceAutobus.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package ReseauTransport;
+import ReseauRoutier.Emplacement;
+import Utilitaire.Temps;
 
 import ReseauRoutier.Emplacement;
 import Utilitaire.Distribution;
@@ -21,16 +23,40 @@ public class SourceAutobus {
     private int m_capaciteMax = 50;
     private Distribution m_distibutionFrequence;
     private Temps m_frequence;
+    private Temps m_tempsAttenteinitial;
+    private Temps m_tempsAvantApparition;
     
-    public void genererAutobus(){
-        Autobus nouvelAutobus = new Autobus(m_emplacement, m_capaciteMax, 
-                                            genererBusID(), calculerTempsApparition(), 
-                                            estSurArret());
-        m_circuit.assignerTrajetAutobus(nouvelAutobus);
-        m_circuit.ajouterAutobus(nouvelAutobus);
+    public SourceAutobus(Emplacement p_emplacement, Circuit p_circuit, String p_nomSource, Distribution p_distribution,  Temps p_tempsAttenteinitial){
+        m_emplacement = p_emplacement;
+        m_circuit = p_circuit;
+        m_nomSource = p_nomSource;
+        m_distibutionFrequence = p_distribution;
+        m_tempsAttenteinitial = p_tempsAttenteinitial;
+        m_tempsAvantApparition = m_tempsAttenteinitial;
     }
     
-    public String genererBusID(){
+    public void miseAjoutTempsRestant(Temps p_deltatT){
+        // Le temps avant le prochain ajout d'autobus diminu selon le deltatT
+        double tmp = m_tempsAvantApparition.getTemps() - p_deltatT.getTemps();
+        m_tempsAvantApparition = new Temps(tmp);
+    }
+    public void genererAutobus(){
+        //Tant que le temps est négatif ou égale a zéro on pop des autobus pour remettre le temps d'apparition > 0;
+        while(m_tempsAvantApparition.getTemps() <= 0){
+            miseAjourAvantAjout();
+            Autobus nouvelAutobus = new Autobus(m_emplacement, m_capaciteMax,  genererBusID(),tempsApparition() ,estSurArret() );
+            m_circuit.ajouterAutobus(nouvelAutobus);
+            m_circuit.assignerTrajetAutobus(nouvelAutobus);
+            //On update le temps avant apparition. On l'addition de la frequence.
+            double tmp = m_tempsAvantApparition.getTemps() + m_frequence.getTemps();
+            m_tempsAvantApparition = new Temps(tmp);
+            initSourceAutobus();
+        }
+    }
+    public int getNbAutobus(){
+        return m_nbAutobusGeneres;
+    }
+    private String genererBusID(){
         m_nbAutobusGeneres++;
         return m_nomSource + Integer.toString(m_nbAutobusGeneres);
     }
@@ -39,8 +65,10 @@ public class SourceAutobus {
         return m_emplacement.equals(m_circuit.getListeArretTrajet().getFirst().getArret().getEmplacement()); 
     }
     
-    public Temps calculerTempsApparition(){
-        //TODO
+    private Temps tempsApparition(){
+       double tmp =  m_tempsAttenteinitial.getTemps()+ (m_frequence.getTemps() * m_nbAutobusGeneres);
+        Temps tmo = new Temps(tmp);
+        return tmo;
     }
     
     public void setCapaciteMax(int capacite){
@@ -48,6 +76,11 @@ public class SourceAutobus {
     }
     
     public void initSourceAutobus(){
-        m_frequence = m_distibutionFrequence.pigerTemps();
+        m_frequence  = m_distibutionFrequence.pigerTemps();
+    }
+    private void miseAjourAvantAjout(){
+        //met à jour le nombre d'autobus et on pige un nouveau temps de distribution
+       // m_frequence  = m_distibutionFrequence.pigerTemps();
+        m_nbAutobusGeneres++;
     }
 }
