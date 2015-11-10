63a5f50e758e0e9bff930e73d9f9b7bd436f8a56
Vincent Martel <Vincent Martel@P8Z68-V>
2015-11-09 21:11:09 -0500

    yoyo

--- SimulatHEURE/src/ReseauRoutier/Intersection.java
+++ SimulatHEURE/src/ReseauRoutier/Intersection.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ReseauRoutier;
 
-import Utilitaire.Distribution;
 import java.util.LinkedList;
-import Utilitaire.Temps;
+import java.awt.geom.Point2D;
 
 /**
  *
@@ -15,25 +9,30 @@ import Utilitaire.Temps;
  */
 public class Intersection {
     private String m_name = "";
-    private Position m_position;
-     private LinkedList<Troncon> m_listTroncons = new LinkedList();
-    public Intersection(Position position){
-        m_position = position;
+    private Point2D.Float m_position;
+    private LinkedList<Troncon> m_listeTroncons = new LinkedList();
+    
+    public Intersection(Point2D.Float position){
+        m_position = position;        
     }
+    
     public void  setName(String name){
         m_name = name;
     }
+    
     public String getName(){
         return m_name;
     }
-     public Position getPosition(){
+    
+     public Point2D.Float getPosition(){
         return m_position;
     }
-     public  LinkedList<Troncon> getListTroncons(){
-         return m_listTroncons;
-     }
+     
+     public  LinkedList<Troncon> getListeTroncons(){
+         return m_listeTroncons;
+    }
+     
      public void ajouterTroncons(Troncon troncon ){
-         m_listTroncons.add( troncon);
-     }
-    
+         m_listeTroncons.add(troncon);
+    }
 }
