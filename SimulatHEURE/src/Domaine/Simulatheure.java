7518c751574c84289d0b012dacaac5215ff99fcf
nasim80 <n.s222att@hotmail.com>
2015-11-26 18:18:39 -0500

    che pas

--- SimulatHEURE/src/Domaine/Simulatheure.java
+++ SimulatHEURE/src/Domaine/Simulatheure.java
@@ -36,7 +36,7 @@ public class Simulatheure {
     private Trajet m_trajet_temp = new Trajet();
     private Boolean m_modeNouvelArret = true;
     private Arret m_arret_temp = new Arret();
-    private Boolean m_dijkstra = true;
+    private Boolean m_dijkstra = false;
 
     private LinkedList<BesoinTransport> m_listBesoins = new LinkedList<>();
 
@@ -266,7 +266,7 @@ public class Simulatheure {
 
     public void cancellerCircuit() {
         deselectionnerRoutier();
-        deselectionnerTransport();
+        //deselectionnerTransport();
         m_reseauRoutier.desuggererTout();
         m_trajet_temp = new Trajet();
         m_arret_temp = new Arret();
