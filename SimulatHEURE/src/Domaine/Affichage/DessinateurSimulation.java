7df833fb750b3e6a923f2af1e2da935e9d1f0d1f
nasim80 <n.s222att@hotmail.com>
2015-12-10 10:36:12 -0500

    c bo

--- SimulatHEURE/src/Domaine/Affichage/DessinateurSimulation.java
+++ SimulatHEURE/src/Domaine/Affichage/DessinateurSimulation.java
@@ -15,12 +15,17 @@ import Domaine.ReseauTransport.*;
 import java.awt.Color;
 
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.font.GlyphVector;
 import java.awt.geom.Ellipse2D;
+import java.io.File;
+import java.io.IOException;
 
 import java.util.LinkedList;
+import javax.imageio.ImageIO;
+import javax.swing.text.Position;
 
 //import java.awt.Image;
 //import java.io.File;
@@ -62,18 +67,20 @@ public class DessinateurSimulation {
                 Point2D.Float position = em.calculPosition(p_echelle);
                 
                 float x = position.x - Autobus.LARGEUR / 2 / p_echelle;
-                float y = position.y - Autobus.HAUTEUR / 2 / p_echelle;
+                float y = position.y - Autobus.HAUTEUR / p_echelle;
                 float largeur = Autobus.LARGEUR / p_echelle;
                 float hauteur = Autobus.HAUTEUR / p_echelle;
                 
 //                int int1 = (int) x;
 //                int int2 = (int) y;
 //                dessinerBus(p_g, int1, int2, p_echelle);
-                p_g.fill(new RoundRectangle2D.Float(x, y, largeur, hauteur, hauteur / 2, hauteur / 2));
-                p_g.setColor(Color.WHITE);
-                p_g.scale(1 / p_echelle, 1 / p_echelle);
-                p_g.drawGlyphVector(noCircuit, (x + largeur / 2) * p_echelle, (y + hauteur / 2) * p_echelle);
-                p_g.scale(p_echelle, p_echelle);
+                //p_g.fill(new RoundRectangle2D.Float(x, y, largeur, hauteur, hauteur / 2, hauteur / 2));
+                p_g.setColor(Color.BLACK);
+                dessinerBus(p_g, position, p_echelle, noCircuit);
+                //p_g.scale(1 / p_echelle, 1 / p_echelle);
+               // p_g.drawGlyphVector(noCircuit, (x + largeur / 2) * p_echelle, (y + hauteur / 2) * p_echelle);
+                //p_g.scale(p_echelle, p_echelle);
+                
             }
         }
     }
@@ -87,8 +94,10 @@ public class DessinateurSimulation {
                 float x = position.x -   individu.RAYON / p_echelle;
                 float y = position.y -   individu.RAYON / p_echelle;
                 float diametre = 2 *   individu.RAYON / p_echelle;
+                
                 p_g.setColor(Color.CYAN);
-                p_g.fill(new Ellipse2D.Float(x, y, diametre, diametre));
+                dessinerPietons(p_g,position,p_echelle);
+                //p_g.fill(new Ellipse2D.Float(x, y, diametre, diametre));
             }
             
         }
@@ -112,19 +121,57 @@ public class DessinateurSimulation {
 
         }
     }
-//    private void dessinerBus(Graphics2D p_g, int p_x, int p_y, float p_echelle){
-//        try{
-//                Image image = ImageIO.read(new File("src/bus.gif"));
-//                float hauteurDefault = 30/p_echelle;
-//                float x = p_x -hauteurDefault;
-//                float y = p_y -hauteurDefault/2;
-//                Graphics2D ii =(Graphics2D )image.getGraphics();
-//               Image im =  image.getScaledInstance((int)(2*hauteurDefault),(int)hauteurDefault, Image.SCALE_SMOOTH);
-//                p_g.drawImage(im, (int)x,(int) y, null);
-//                
-//        }
-//        catch(IOException ex){
-//                    System.out.println("nop");
-//        }
-//    }
+    private void dessinerBus(Graphics2D p_g,Point2D.Float p_position, float p_echelle, GlyphVector p_noCircuit){
+        try{
+                Image image = ImageIO.read(new File("src/bus.gif"));
+                
+                float hauteur = 40;
+                float largeur = 42;
+                float x = p_position.x - largeur / p_echelle;
+                float y = p_position.y - hauteur / p_echelle;
+                float largeurFinal = largeur / p_echelle;
+                float hauteurFinal = hauteur / p_echelle;
+
+                int xGF = (int)((x + largeurFinal) * p_echelle);
+                int yGF = (int)((y + hauteurFinal/1.2) * p_echelle);
+                
+           
+                int xF = (int)((x + largeurFinal / 2) * p_echelle);
+                int yF = (int)((y + hauteurFinal / 2) * p_echelle);
+                //Image im =  image.getScaledInstance((int)(largeur),(int)hauteur, Image.SCALE_DEFAULT);
+                p_g.scale(1 / p_echelle, 1 / p_echelle);
+                p_g.drawImage(image, xF,yF, null);
+                p_g.drawGlyphVector(p_noCircuit,xGF,yGF);
+                p_g.scale(p_echelle, p_echelle);
+              
+                
+        }
+        catch(IOException ex){
+                    System.out.println("nop");
+        }
+    }
+    
+    private void dessinerPietons(Graphics2D p_g,Point2D.Float p_position, float p_echelle){
+        try{
+                Image image = ImageIO.read(new File("src/pieton.gif"));
+                
+                float hauteur = 33;
+                float largeur = 23;
+                float x = p_position.x - largeur / p_echelle;
+                float y = p_position.y - hauteur / p_echelle;
+                float largeurFinal = largeur / p_echelle;
+                float hauteurFinal = hauteur / p_echelle;
+                int xF = (int)((x + largeurFinal / 2) * p_echelle);
+                int yF = (int)((y + hauteurFinal / 2) * p_echelle);
+                //Image im =  image.getScaledInstance((int)(largeur),(int)hauteur, Image.SCALE_DEFAULT);
+                p_g.scale(1 / p_echelle, 1 / p_echelle);
+                p_g.drawImage(image, xF,yF, null);
+                p_g.scale(p_echelle, p_echelle);
+              
+                
+        }
+        catch(IOException ex){
+                    System.out.println("nop");
+        }
+    }
 }
