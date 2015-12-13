84340794a5bb9482c9c4e0cd1548305b8c9a8dcb
Louis Fortier-Dubois <louisfd94@gmail.com>
2015-12-13 11:56:35 -0500

    courriel du prof done

--- SimulatHEURE/src/Domaine/Utilitaire/AlternateurCouleurs.java
+++ SimulatHEURE/src/Domaine/Utilitaire/AlternateurCouleurs.java
@@ -15,10 +15,10 @@ public class AlternateurCouleurs { //SINGLETON ALRIGHT
    private static AlternateurCouleurs m_this = new AlternateurCouleurs( );
    private final static Integer[] m_couleurs = new Integer[] { 
         0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF,
-        0xC00000, 0x00C000, 0x0000C0, 0xC0C000, 0xC000C0, 0x00C0C0,
+        0xB00000, 0x00B000, 0x0000B0, 0xB0B000, 0xB000B0, 0x00B0B0,
+        0xD00000, 0x00D000, 0x0000D0, 0xD0D000, 0xD000D0, 0x00D0D0,
         0xA00000, 0x00A000, 0x0000A0, 0xA0A000, 0xA000A0, 0x00A0A0,
         0xE00000, 0x00E000, 0x0000E0, 0xE0E000, 0xE000E0, 0x00E0E0,
-        0x990000, 0x009900, 0x009900, 0x909000, 0x900090, 0x009090, 
     };
     private static Integer m_couleurIndex = (int)(Math.random() * ((m_couleurs.length) + 1));
 
@@ -36,7 +36,7 @@ public class AlternateurCouleurs { //SINGLETON ALRIGHT
    
    public static Color getCouleurCircuit(){
        Integer couleur = getCouleur();
-       return new Color(couleur & 0x808080);
+       return new Color(couleur & 0x7F7F7F);
    }
    
    public static Color getCouleurItineraire(){
